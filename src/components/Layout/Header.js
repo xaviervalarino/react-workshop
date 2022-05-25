dfc37477d5b446d77faed1756f5cb7d6f96b74eb
Xavier Valarino <xvalarino@gmail.com>
2022-05-25 01:03:53 -0500

    Add Header component

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,18 @@
+import classes from "./Header.module.css";
+import sandwichImg from "../../assets/chicken-waldorf-sandwiches-hero.webp";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>Sandwich Shop</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={sandwichImg} alt="Waldorf chicken salad sandwich" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
