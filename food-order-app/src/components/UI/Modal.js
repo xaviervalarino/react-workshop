0ef0be02b694c377192a101c4341bc25b286eee5
Xavier Valarino <xvalarino@gmail.com>
2022-08-13 19:46:32 -0700

    Add modal overlay component

---
+++ food-order-app/src/components/UI/Modal.js
@@ -0,0 +1,27 @@
+import { createPortal } from "react-dom";
+import classes from "./Modal.module.css";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop}></div>;
+};
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("overlay");
+
+export default function Modal(props) {
+  return (
+    <>
+      {createPortal(<Backdrop />, portalElement)}
+      {createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </>
+  );
+}
