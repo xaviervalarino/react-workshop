9aacbfa88ae39c54fcd5a4755b074048240e5a38
Xavier Valarino <xvalarino@gmail.com>
2022-08-14 00:14:26 -0700

    Use context to populate Cart items

--- food-order-app/src/components/Cart/Cart.js
+++ food-order-app/src/components/Cart/Cart.js
@@ -1,22 +1,25 @@
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 
 export default function Cart(props) {
+  const cartCtx = useContext(CartContext);
+  const totalAmountStr = `$${cartCtx.totalAmount.toFixed(2)}`;
   const cartItems = (
-    <ul>
-      {[{ id: "c1", name: "Tuna fish", amount: 2, price: 12.99 }].map(
-        (item) => (
-          <li key={`${item.id}-${i}`}>{item.name}</li>
-        )
-      )}
+    <ul className={classes["cart-items"]}>
+      {cartCtx.items.map((item, i) => (
+        <li key={`${item.id}-${i}`}>{item.name}</li>
+      ))}
     </ul>
   );
+
   return (
     <Modal onClose={props.onClose}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmountStr}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onClose}>
