a3c6d5170a790a3df10bec744d907449277513ca
Xavier Valarino <xvalarino@gmail.com>
2022-08-14 16:05:11 -0700

    Update CartItem component and styles

--- food-order-app/src/components/Cart/CartItem.js
+++ food-order-app/src/components/Cart/CartItem.js
@@ -1,20 +1,18 @@
 import classes from "./CartItem.module.css";
 
 export default function CartItem(props) {
-  const price = `$${props.price}`;
+  const priceStr = `$${props.price.toFixed(2)}`;
   return (
     <li key={props.id} className={classes["cart-item"]}>
-      <div>
-        <h3>{props.name}</h3>
         <div className={classes.summary}>
-          <span className={classes.price}>{props.price}</span>
-          <span className={classes.amount}>x {props.amount}</span>
+          <h3>{props.name}</h3>
+          <span className={classes.price}>{priceStr}</span>
+          <span className={classes.amount}>{props.amount}</span>
         </div>
-          <div className={classes.actions}>
+        <div className={classes.actions}>
           <button onClick={props.onRemove}>-</button>
           <button onClick={props.onAdd}>+</button>
         </div>
-      </div>
     </li>
   );
 }
