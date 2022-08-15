387b51d3420bd4382696fcbc3d797c7d17cad2d0
Xavier Valarino <xvalarino@gmail.com>
2022-08-14 17:19:24 -0700

    Animate header cart button when items are added

--- food-order-app/src/components/Layout/HeaderCartButton.js
+++ food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,29 @@
-import { useContext } from "react";
+import { useEffect, useContext, useState } from "react";
 
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnBump, setBtnBump] = useState(false);
   const cartCtx = useContext(CartContext);
   const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
 
+  const { items } = cartCtx;
+  const btnClasses = `${classes.button} ${btnBump ? classes.bump : ""}`;
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnBump(true);
+    const timer = setTimeout(() => setBtnBump(false), 300);
+    return () => clearTimeout(timer);
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
