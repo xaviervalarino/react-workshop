9f10f24ececa7aab2091e7c90e9398c01f46555e
Xavier Valarino <xvalarino@gmail.com>
2022-08-23 16:50:39 -0700

    Ensure fetch function is not recreated

--- database-connection/src/App.jsx
+++ database-connection/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import MovieList from "./components/MovieList";
 
 export default function App() {
@@ -6,22 +6,27 @@ export default function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
-    setError(null)
+    setError(null);
     try {
       const res = await fetch("https://swapi.dev/api/films/");
       if (!res.ok) {
         throw new Error("Something went wrong.");
       }
       const data = await res.json();
-      console.log(data.results);
+      console.log("fetch results", data.results);
       setMovies(data.results);
     } catch (error) {
       setError(error.message);
     }
     setIsLoading(false);
-  }
+  }, []);
+
+  useEffect(() => {
+    console.log("useEffect fired");
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
   let content = <p>No movies.</p>;
   if (movies.length) {
@@ -37,7 +42,6 @@ export default function App() {
   return (
     <div style={{ width: "60ch" }}>
       <h1>Movie List</h1>
-      <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       {content}
     </div>
   );
