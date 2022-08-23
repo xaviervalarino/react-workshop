8276e0f553fd73cbd105958919974a9de719f531
Xavier Valarino <xvalarino@gmail.com>
2022-08-23 16:43:13 -0700

    Add error handling

--- database-connection/src/App.jsx
+++ database-connection/src/App.jsx
@@ -4,21 +4,41 @@ import MovieList from "./components/MovieList";
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   async function fetchMoviesHandler() {
     setIsLoading(true);
-    const res = await fetch("https://swapi.dev/api/films/");
-    const data = await res.json();
-    console.log(data.results);
-    setMovies(data.results);
+    setError(null)
+    try {
+      const res = await fetch("https://swapi.dev/api/films/");
+      if (!res.ok) {
+        throw new Error("Something went wrong.");
+      }
+      const data = await res.json();
+      console.log(data.results);
+      setMovies(data.results);
+    } catch (error) {
+      setError(error.message);
+    }
     setIsLoading(false);
+  }
+
+  let content = <p>No movies.</p>;
+  if (movies.length) {
+    content = <MovieList movies={movies} />;
+  }
+  if (error) {
+    content = <p style={{ color: "crimson", fontWeight: "bold" }}>{error}</p>;
+  }
+  if (isLoading) {
+    content = <p>Loading...</p>;
   }
 
   return (
     <div style={{ width: "60ch" }}>
       <h1>Movie List</h1>
       <button onClick={fetchMoviesHandler}>Fetch Movies</button>
-      {(!isLoading && <MovieList movies={movies} />) || <p>Loading...</p>}
+      {content}
     </div>
   );
 }
