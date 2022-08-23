811180633e91a1b94b4d6a68b9e778498f297f10
Xavier Valarino <xvalarino@gmail.com>
2022-08-23 14:20:26 -0700

    Use `componentWillUnmount()` method

--- class-based-components/src/components/User.jsx
+++ class-based-components/src/components/User.jsx
@@ -1,6 +1,10 @@
 import { Component } from "react";
 
 export default class User extends Component {
+  // Similar to `useEffect()` clean up function
+  componentWillUnmount() {
+    console.log(`User "${this.props.name}" unmounted`)
+  }
   render() {
     return <li key={this.props.id}>{this.props.name}</li>;
   }
