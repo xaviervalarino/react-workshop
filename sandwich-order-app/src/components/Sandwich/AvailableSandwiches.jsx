e501ec5045591d42038156d9e32fea5cc1a987b9
Xavier Valarino <xvalarino@gmail.com>
2022-09-12 22:06:27 -0700

    Change data structure in preparation for Firebase
    Going to be moving the dummy data to Firebase DB, which is a key/value
    store instead of an array.

--- sandwich-order-app/src/components/Sandwich/AvailableSandwiches.jsx
+++ sandwich-order-app/src/components/Sandwich/AvailableSandwiches.jsx
@@ -1,54 +1,48 @@
 import Card from "../UI/Card";
 import SandwichItem from "./SandwichItem/SandwichItem";
-import classes from "./AvailableSandwiches.module.css"
+import classes from "./AvailableSandwiches.module.css";
 
-const DUMMY_SANDWICHES = [
-  {
-    id: "m1",
+const DUMMY_SANDWICHES = {
+  m1: {
     name: "Pastami",
     description: "Pastami sandwich",
     price: 12.99,
   },
-  {
-    id: "m2",
+  m2: {
     name: "Waldorf Chicken Salad",
     description: "Waldorf Chicken Salad sandwich",
     price: 11.99,
   },
-  {
-    id: "m3",
+  m3: {
     name: "Roast Beef",
     description: "Roast Beef sandwich",
     price: 13.99,
   },
-  {
-    id: "m4",
+  m4: {
     name: "Vegetarian",
     description: "Vegetarian sandwich",
     price: 10.99,
   },
-  {
-    id: "m5",
+  m5: {
     name: "Breakfast",
     description: "Breakfast sandwich—served all day!",
     price: 11.99,
   },
-];
+};
 
 export default function AvailableSandwiches() {
-  const sandwichList = DUMMY_SANDWICHES.map(
-    ({ id, name, description, price }) => {
-      return (
-        <SandwichItem
-          id={id}
-          key={id}
-          name={name}
-          description={description}
-          price={price}
-        />
-      );
-    }
-  );
+  const sandwichList = Object.keys(DUMMY_SANDWICHES).map((id) => {
+    const { name, description, price } = DUMMY_SANDWICHES[id];
+    return (
+      <SandwichItem
+        id={id}
+        key={id}
+        name={name}
+        description={description}
+        price={price}
+      />
+    );
+  });
   return (
     <section>
       <Card>
