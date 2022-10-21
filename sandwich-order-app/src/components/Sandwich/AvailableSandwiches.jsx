2d8c241c3e15a4e5e3e3a5e83a7587b87cdfdf32
Xavier Valarino <xvalarino@gmail.com>
2022-10-21 12:07:27 -0700

    Add HTTP error state

--- sandwich-order-app/src/components/Sandwich/AvailableSandwiches.jsx
+++ sandwich-order-app/src/components/Sandwich/AvailableSandwiches.jsx
@@ -7,17 +7,26 @@ import classes from "./AvailableSandwiches.module.css";
 export default function AvailableSandwiches() {
   const [sandwiches, setSandwiches] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch(
-        "https://react-workshop-679c8-default-rtdb.firebaseio.com/Sandwiches.json"
-      );
-      const data = await res.json();
-      setSandwiches(data);
-    };
-    fetchData();
-    setIsLoading(false);
+      const fetchData = async () => {
+        const res = await fetch(
+          "https://react-workshop-679c8-default-rtdb.firebaseio.com/Sandwiches.json"
+        );
+        if (!res.ok) {
+          throw new Error("Something went wrong!");
+        }
+        const data = await res.json();
+        setSandwiches(data);
+        setIsLoading(false);
+      };
+      fetchData()
+        .then()
+        .catch((err) => {
+          setIsLoading(false);
+          setHttpError(err.message);
+        });
   }, []);
 
   const sandwichList = Object.keys(sandwiches).map((id) => {
@@ -33,13 +42,24 @@ export default function AvailableSandwiches() {
     );
   });
 
+  let content = "";
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+  else if (httpError) {
+    content = (
+      <div className={classes.error}>
+        <h3>Error</h3>
+        <p >{httpError}</p>
+      </div>
+    );
+  } else {
+    content = <ul className={classes.list}>{sandwichList}</ul>;
+  }
+
   return (
     <section>
-      <Card>
-        {(isLoading && <p>Loading...</p>) || (
-          <ul className={classes.list}>{sandwichList}</ul>
-        )}
-      </Card>
+      <Card>{content}</Card>
     </section>
   );
 }
