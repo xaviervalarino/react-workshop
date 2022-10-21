1ea08896c9fbd5263909f3bac7e8208a28cd2398
Xavier Valarino <xvalarino@gmail.com>
2022-10-21 14:16:26 -0700

    Add basic form validation on submission

--- sandwich-order-app/src/components/Cart/Checkout.jsx
+++ sandwich-order-app/src/components/Cart/Checkout.jsx
@@ -1,24 +1,65 @@
+import { useRef, useState } from "react";
 import TextField from "../UI/TextField";
 import Button, { ButtonGroup } from "../UI/Button";
 import classes from "./Checkout.module.css";
 
+const isEmpty = (value) => value.trim().length === 0;
+const isFiveChars = (value) => value.trim().length === 5;
+
 export default function Checkout(props) {
+  const [formInputsValid, setFormInputsValid] = useState({
+    name: true,
+    address: true,
+    city: true,
+    postalCode: true,
+  });
+  const nameInputRef = useRef();
+  const addressInputRef = useRef();
+  const cityInputRef = useRef();
+  const postalInputRef = useRef();
+
   const confirmHandler = (e) => {
     e.preventDefault();
+    setFormInputsValid({
+      name: !isEmpty(nameInputRef.current.value),
+      address: !isEmpty(addressInputRef.current.value),
+      city: !isEmpty(cityInputRef.current.value),
+      postalCode: isFiveChars(postalInputRef.current.value),
+    });
   };
 
   return (
     <form>
-      <TextField id="name" title="Your Name" />
-      <TextField id="address" title="Street Address" />
+      <TextField
+        id="name"
+        title="Your Name"
+        error={!formInputsValid.name && "Please enter a valid name"}
+        ref={nameInputRef}
+      />
+      <TextField
+        id="address"
+        title="Street Address"
+        error={!formInputsValid.address && "Please enter a valid address"}
+        ref={addressInputRef}
+      />
       <div className={classes.inline}>
-        <TextField id="city" title="City" />
-        <TextField id="postal" title="Postal Code" />
+        <TextField
+          id="city"
+          title="City"
+          error={!formInputsValid.city && "Please enter a valid city"}
+          ref={cityInputRef}
+        />
+        <TextField
+          id="postal"
+          title="Postal Code"
+          error={!formInputsValid.postalCode && "Please enter a valid postal code"}
+          ref={postalInputRef}
+        />
       </div>
 
       <ButtonGroup>
         <Button alt type="button" onClick={props.onCancel} text="Cancel" />
-        <Button text="Confirm" />
+        <Button text="Confirm" onClick={confirmHandler} />
       </ButtonGroup>
     </form>
   );
--- sandwich-order-app/src/components/UI/TextField.jsx
+++ sandwich-order-app/src/components/UI/TextField.jsx
@@ -2,9 +2,10 @@ import { forwardRef } from "react";
 import classes from "./TextField.module.css";
 
 const TextField = forwardRef((props, ref) => (
-  <div className={classes.control}>
+  <div className={`${classes.control} ${props.error && classes.error}`}>
     <label htmlFor={props.id}>{props.title}</label>
     <input type="text" id={props.id} ref={ref} />
+    {props.error && <p>{props.error}</p>}
   </div>
 ));
 
