fb604003fc043e268b38f8c06db2c48d6a70dbfa
Xavier Valarino <xvalarino@gmail.com>
2022-10-21 18:57:38 -0700

    Add input to change increment/decrement amount

--- redux-basics/src/App.tsx
+++ redux-basics/src/App.tsx
@@ -1,11 +1,21 @@
+import { useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { CountState } from "./store";
 
 export default function App() {
+  const amountRef = useRef<HTMLInputElement>(null);
   const dispatch = useDispatch();
   const counter = useSelector((state: CountState) => state.count);
+  const amount = useSelector((state: CountState) => state.amount);
+
   const decrementHandler = () => dispatch({ type: "decrement" });
   const incrementHandler = () => dispatch({ type: "increment" });
+  const changeAmountHandler = () => {
+    dispatch({
+      type: "change_amount",
+      amount: amountRef.current?.value || 1,
+    });
+  };
 
   return (
     <div
@@ -21,6 +31,28 @@ export default function App() {
       <div style={{ display: "flex", gap: "0.5rem" }}>
         <button onClick={decrementHandler}>-</button>
         <button onClick={incrementHandler}>+</button>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          margin: "1rem",
+        }}
+      >
+        <label htmlFor="count">Count by:</label>
+        <input
+          type="number"
+          id="count"
+          defaultValue={amount}
+          onChange={changeAmountHandler}
+          ref={amountRef}
+          style={{
+            width: "6rem",
+            padding: "0.5rem",
+            borderRadius: "4px",
+            border: "1px solid #999",
+          }}
+        />
       </div>
     </div>
   );
--- redux-basics/src/store/index.ts
+++ redux-basics/src/store/index.ts
@@ -1,20 +1,33 @@
 import { createStore } from "redux";
-import { Action } from "redux";
 
 export interface CountState {
-  count: number
+  count: number;
+  amount: number;
 }
 
-const countReducer = (state: CountState = { count: 0 }, action: Action) => {
+interface Action {
+  readonly type: string;
+  readonly amount: number;
+}
+
+const initial: CountState = {
+  count: 0,
+  amount: 1,
+};
+
+const countReducer = (state: CountState = initial, action: Action) => {
   if (action.type === "increment") {
-    return { count: state.count + 1 };
+    return { ...state, count: state.count + state.amount};
   }
   if (action.type === "decrement") {
-    return { count: state.count - 1 };
-
+    return { ...state, count: state.count - state.amount};
+  }
+  if (action.type === "change_amount") {
+    return { ...state, amount: +action.amount}
   }
   return state;
 };
+
 const store = createStore(countReducer);
 
 export default store;
